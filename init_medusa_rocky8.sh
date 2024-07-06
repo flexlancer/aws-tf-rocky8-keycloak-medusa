74728ee9e834dd1ebb2dcecfbf3e81b8170fc335
flexlancer-ch <62950511+flexlancer@users.noreply.github.com>
2024-07-05 22:20:11 -0500

    Update init_medusa_rocky8.sh

--- init_medusa_rocky8.sh
+++ init_medusa_rocky8.sh
@@ -14,12 +14,10 @@ sudo systemctl enable postgresql
 sudo systemctl status postgresql
 
 echo "Add entries in /var/lib/pgsql/data/pg_hba.conf to allow md5 auth for all & postgres User"
+echo "Add Listener entry in /var/lib/pgsql/data/postgresql.conf "
 sudo su
 echo 'host      all           all             0.0.0.0/0               md5' >> /var/lib/pgsql/data/pg_hba.conf
 echo 'host      all           postgres        127.0.0.1/32            md5' >> /var/lib/pgsql/data/pg_hba.conf
-
-echo "Add Listener entry in /var/lib/pgsql/data/postgresql.conf "
-sudo su
 echo "listen_addresses = '*'" >> /var/lib/pgsql/data/postgresql.conf
 
 echo "Create postgres components"
