c4f06b2107158eb1affc3311ffb00c59c8ae6f13
flexlancer-ch <62950511+flexlancer@users.noreply.github.com>
2024-07-05 23:19:58 -0500

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -54,6 +54,19 @@ output "keycloak_master_instance_public_dns" {
   value = aws_instance.keycloak-master-instance.public_dns
 }
 
+resource "local_file" "tf_localfile_output" {
+  content         = <<-EOT
+  [keycloak-master-instance]
+  ${aws_instance.keycloak-master-instance.private_ip}
+  [keycloak-master-instance]
+  ${aws_instance.keycloak-master-instance.public_dns} 
+  EOT
+  file_permission = 0400
+  filename        = format("%s/%s", abspath(path.root), "keycloak_ips.txt")
+}
+
+
+
 
 /*
 resource "aws_instance" "keycloak-worker-instance" {
