bc2896272f745de1a2ac48d9408debe5e44bc656
flexlancer-ch <62950511+flexlancer@users.noreply.github.com>
2024-07-05 23:19:32 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,3 @@
-resource "local_file" "tf_localfile_output" {
-  content         = <<-EOT
-  [keycloak-master-instance]
-  ${aws_instance.keycloak-master-instance.private_ip}
-  [keycloak-master-instance]
-  ${aws_instance.keycloak-master-instance.public_dns} 
-  EOT
-  file_permission = 0400
-  filename        = format("%s/%s", abspath(path.root), "keycloak_ips.txt")
-}
-
 output "public_ssh_controller_key" {
   value = tls_private_key.controller_private_key.public_key_openssh
 }
